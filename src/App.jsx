8eaaf531f0792102045fb9561c22df8c1fc4acb9
Feniuk <143640539+Feniuk@users.noreply.github.com>
2024-03-15 19:51:06 +0100

    project set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,7 @@
-import userData from "./userData.json";
-import Profile from "./components/Profile/Profile";
 import "./components/App.css";
-import friends from "./friends.json";
-import FriendList from "./components/Friendlist/FriendList";
-import transactions from "./transaction.json";
-import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
 
 const App = () => {
-  return (
-    <>
-      <Profile
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        image={userData.avatar}
-        stats={userData.stats}
-      />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
-    </>
-  );
+  return <>Hello</>;
 };
 
 export default App;
