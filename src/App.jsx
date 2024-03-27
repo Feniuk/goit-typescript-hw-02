f3f6743fc84afd3a66ca5318541a0d8bdf84031b
Feniuk <143640539+Feniuk@users.noreply.github.com>
2024-03-27 21:37:48 +0100

    project set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,8 @@
-import { useState, useEffect } from "react";
-import "./components/App.css";
-import ContactList from "./components/ContactList/ContactList";
-import initialContacts from "./initialContacts";
-import SearchBox from "./components/SearchBox/SearchBox";
-import ContactForm from "./components/ContactForm/ContactForm";
-
 function App() {
-  const savedContacts =
-    JSON.parse(window.localStorage.getItem("saved-contacts")) ||
-    initialContacts;
-  const [contacts, setContacts] = useState(savedContacts);
-  const [filter, setFilter] = useState("");
-
-  const addContact = (newContact) => {
-    setContacts((prevContacts) => {
-      return [...prevContacts, newContact];
-    });
-  };
-
-  useEffect(() => {
-    window.localStorage.setItem("saved-contacts", JSON.stringify(contacts));
-  }, [contacts]);
-
-  const deleteContact = (id) => {
-    setContacts((prevContacts) => {
-      return prevContacts.filter((contact) => contact.id !== id);
-    });
-  };
-
-  const findContact = contacts.filter((contact) =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
-
   return (
     <>
       <div>
-        <h1>Phonebook</h1>
-        <ContactForm onAdd={addContact} />
-        <SearchBox value={filter} onFilter={setFilter} />
-        <ContactList contacts={findContact} onDelete={deleteContact} />
+        <>Zdarova!</>
       </div>
     </>
   );
